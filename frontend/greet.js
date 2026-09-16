faf0b1c1ddd831d96a486959cd2cdc2fd6a386b9
Gunnar Hilling <gunnar@hilling.de>
2026-09-16 13:54:26 +0200

    initial

---
+++ frontend/greet.js
@@ -0,0 +1,17 @@
+function greet(domInput, domElement) {
+    const api = greet.api || getGreeting;
+    return api(domInput.value).then(text => {
+        domElement.innerHTML = text;
+    });
+}
+
+
+function getGreeting(name) {
+    const url = "http://localhost:3000?name=" + name
+    return fetch(url)
+        .then(response => response.text());
+}
+
+if (typeof module !== "undefined") {
+    module.exports = greet;
+}
